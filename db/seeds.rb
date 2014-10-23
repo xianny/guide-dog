5ab3fae95b7811748ef17911c5dfa04763e944de
Xianny <xianny@gmail.com>
2014-10-23 15:46:41 -0700

    seeds.rb is working

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,36 +11,6 @@ FactoryGirl.define do
 end
 
 
-# FactoryGirl.define do
-#   factory :tag do
-#     name Faker::Lorem.word
-#   end
-# end
-
-# 200.times do 
-#   FactoryGirl.create(:tag)
-# end
-
-# FactoryGirl.define do
-#   factory :activity do
-#     title { Faker::Lorem.sentence }
-#     # at_home boolean
-#     # social boolean
-#     cost { "#{rand(4)}" }
-#     location "#{Faker::Address.city} #{Faker::Address.state_abbr}"
-#     content { Faker::Lorem.paragraph }
-#   end
-# end
-
-
-# FactoryGirl.define do
-#   factory :review do
-#     comment { Faker::Lorem.paragraph }
-#     rating { "#{rand(5) + 1}" }
-#     activity
-#   end
-# end
-
 ## START CREATION
 ##
 
@@ -50,41 +20,38 @@ end
 end
 
 # Creates 10 new tags
-Tag.create(name:'')
-Tag.create(name:'')
-Tag.create(name:'')
-Tag.create(name:'')
-Tag.create(name:'')
-Tag.create(name:'')
-Tag.create(name:'')
-Tag.create(name:'')
-Tag.create(name:'')
-Tag.create(name:'')
+Tag.create(name:'food')
+Tag.create(name:'music')
+Tag.create(name:'sports')
+Tag.create(name:'shopping')
+Tag.create(name:'lazy')
+Tag.create(name:'adventurous')
+Tag.create(name:'getting_smashed')
+Tag.create(name:'beach')
+Tag.create(name:'ski')
+Tag.create(name:'park')
+@tags = Tag.all
 
 users = User.all
 
 ## Each user posts 2 activities and adds 3 tags to it
 users.each do |user|
-  tags = Tag.all
   2.times do 
     activity = user.activities.create(
       title: "#{Faker::Lorem.sentence}",
       location: "#{Faker::Address.city} #{Faker::Address.state_abbr}",
       cost: rand(4)
     )
-    3.times { activity.tags << tags[rand(tags.count)] }
-    end
     activity.save if activity.valid?
+    3.times { activity.tags << @tags.sample }
   end
 end
 
 ## Each user reviews 5 random activities that they did not author
 users.each do |user|
-  activities = Activity.where("user_id != ?", user.id)
-  
-  5.times do
-    activity = activities[rand(activities.count)]
-    review = activity.reviews.create(
+  activities = Activity.where("user_id != ?", user.id).sample(5)
+  activities.each do |a|
+    review = a.reviews.create(
         comment: "#{Faker::Lorem.paragraph}",
         rating: rand(5) + 1,
         user_id: user.id
@@ -92,10 +59,4 @@ users.each do |user|
     review.save if review.valid?
   end
 end
-
-# users.each do |user|
-#   users.reviews.each do |review|
-#     review.rating += 1
-#   end
-# end
 
