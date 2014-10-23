860b5e4758b6ea0b6f57b1c4eda6e52846ad3ff1
Xianny <xianny@gmail.com>
2014-10-23 16:11:04 -0700

    Templated erb paths

--- app/actions.rb
+++ app/actions.rb
@@ -3,11 +3,88 @@ get '/' do
   erb :index
 end
 
-# User profile page
-get '/user' do
-  erb :'user/index'
+# Display all users
+get '/users' do
+  erb :'users/index'
 end
 
+# Display form to create new user
+get '/users/new' do
+
+end
+
+# Create new user (sign up for acount)
+post '/users' do
+
+end
+
+# Display single user
+get '/users/:u_id' do
+
+end
+
+
+#### ACTIVITIES ####
+#### ---------- ####
+
+
+# Display activities associated with a single user
+get '/users/:u_id/activities' do
+
+end
+
+# Display form to create new activity (by current user)
+get '/users/:u_id/activities/new' do
+
+end
+
+# Create new activity
+post '/users/:u_id/activities' do
+
+end
+
+# Display single activity
+get '/users/:u_id/activities/:a_id' do 
+
+end
+
+#### REVIEWS ####
+#### ------- ####
+
+
+# Display all reviews associated with the activity
+get '/users/:u_id/activities/:a_id/reviews' do
+
+end
+
+# Display form to post new review
+get '/users/:u_id/activities/:a_id/reviews/new' do
+
+end
+
+# Post new review
+post '/users/:u_id/activities/:a_id/reviews' do
+
+end
+
+# Display a single review
+get '/users/:u_id/activities/:a_id/reviews/:r_id' do
+
+end
+
+#### SEARCH FUNCTIONS ####
+#### ---------------- ####
+
+## Display activities by parameters
+# params = {
+#   location: 'string'
+#   cost: in: [0,1,2,3]
+#   at_home: boolean
+#   social: boolean
+#   user_id: integer
+# }
 get '/activities' do
-  erb :'activities/index'
+  # generate collection of activities based on parameters
+  # set as instance variable @activities
+  # call erb: 'activities/index'
 end
